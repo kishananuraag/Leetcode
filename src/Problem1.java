563a2bd2c58dc64d6f84d49c9edfa5448467fe7d
kishan <kishananuraag@gmail.com>
2023-01-14 12:09:57 +0530

    New commit

--- src/Problem1.java
+++ src/Problem1.java
@@ -15,27 +15,27 @@ public class Problem1{
 class Problem1Solution{
 
     public int[] twoSum(int[] nums, int target){
-//        for(int i = 0; i<nums.length; i++)
-//        {
-//            for(int j = i+1; j<nums.length; j++)
-//            {
-//                if(nums[i]+nums[j] == target)
-//                {
-//                    return new int[] {i,j};
-//                }
-//            }
-//        }
-
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++){
-//        for (int num : nums){
-            int num = nums[i];
-            int diff = target - num;
-            if (map.containsKey(diff)){
-                return new int[]{map.get(diff), i};
+        for(int i = 0; i<nums.length; i++)
+        {
+            for(int j = i+1; j<nums.length; j++)
+            {
+                if(nums[i]+nums[j] == target)
+                {
+                    return new int[] {i,j};
+                }
             }
-            map.put(num, i);
         }
+
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//        for (int i = 0; i < nums.length; i++){
+////        for (int num : nums){
+//            int num = nums[i];
+//            int diff = target - num;
+//            if (map.containsKey(diff)){
+//                return new int[]{map.get(diff), i};
+//            }
+//            map.put(num, i);
+//        }
 
      return new int[]{-1, -1};
     }
